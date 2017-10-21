2ef2d96bf88fa072916f4e6b92d30d8a0a01eb32
JhonnyBravo <sanfranceshika5@gmail.com>
2017-10-21 21:42:18 +0900

    modified: get_package.ps1
    ダウンロード対象とするファイルの URL を
    CSV ファイルから取得するように変更。

--- get_package.ps1
+++ get_package.ps1
@@ -2,19 +2,23 @@
     [string]$path
 )
 
-$url=$(Get-Content $path)
+Import-Csv $path | ForEach-Object {
+    If ($_.Title -eq "Windows hosts"){
+        $url=$_.URL
+    }
+}
+
 $dst=$(Split-Path $url -Leaf)
 
 wget $url -OutFile $dst
 
-
 <#
 .SYNOPSIS
-URL リストに記載された URL から現在のディレクトリへパッケージをダウンロードします。
+CSV に記載された URL から現在のディレクトリへパッケージをダウンロードします。
 
 .DESCRIPTION
-URL リストに記載された URL から現在のディレクトリへパッケージをダウンロードします。
+CSV に記載された URL から現在のディレクトリへパッケージをダウンロードします。
 
 .PARAMETER path
-URL リストのパス。
+CSV のパス。
 #>
